81fb2b5b68f5bb3de0db69c0752de59492e16167
rvlb-19 <renatovlbarros19@gmail.com>
2021-02-11 16:03:25 -0300

    Build for windows and unix

--- etc/release.sh
+++ etc/release.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-function backup_release {
-    if [[ (-d "dist") || (-d "build") ||  (-d "release") ]]; then
-        previous_release=release-backup-$(date --iso=seconds)
-        mkdir $previous_release
-        [[ (-d "dist") ]] && mv dist $previous_release
-        [[ (-d "build") ]] && mv build $previous_release
-        [[ (-d "release") ]] && mv release $previous_release
-    fi
-}
+# Backup previous release
+if [[ (-d "dist") || (-d "build") ||  (-d "release") ]]; then
+    previous_release=release-backup-$(date --iso=seconds)
+    mkdir $previous_release
+    [[ (-d "dist") ]] && mv dist $previous_release
+    [[ (-d "build") ]] && mv build $previous_release
+    [[ (-d "release") ]] && mv release $previous_release
+fi
 
-function clean_build {
-    rm -rf build dist
-}
-
-backup_release
-clean_build
+# Delete current build archives
+rm -rf build dist
 mkdir release
 
+# Make sure you're running this inside a virtual environment
 pip install -r ./requirements.txt
 
+# Build wheel
 python setup.py sdist bdist_wheel
+
+# Build tarball
+python setup.py sdist --formats=xztar
+
+# Build zip (for windows)
+python setup.py sdist --formats=zip
 
 mv dist release/pypi
