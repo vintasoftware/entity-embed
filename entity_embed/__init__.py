6d1cd5828f8d8200a4218a4fc1f0ef3c5d322089
rvlb-19 <renatovlbarros19@gmail.com>
2021-02-18 11:46:59 -0300

    Update init to avoid linter issues

--- entity_embed/__init__.py
+++ entity_embed/__init__.py
@@ -1,11 +1,11 @@
 """Top-level package for entity-embed."""
 import logging
 
+from .entity_embed import *  # noqa
+
 __author__ = """Flávio Juvenal"""
 __email__ = "flavio@vinta.com.br"
 __version__ = "0.0.1"
 
 # Good practice: https://docs.python-guide.org/writing/logging/#logging-in-a-library
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-from .entity_embed import *
