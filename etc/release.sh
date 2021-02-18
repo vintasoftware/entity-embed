958f2a324ab3240c53f29faa4ae9f56eca36a944
rvlb-19 <renatovlbarros19@gmail.com>
2021-02-18 15:32:58 -0300

    Remove unused lines for the release script

--- etc/release.sh
+++ etc/release.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 # Backup previous release
-if [[ (-d "dist") || (-d "build") ||  (-d "release") ]]; then
+if [[ (-d "dist") || (-d "build") ]]; then
     previous_release=release-backup-$(date --iso=seconds)
     mkdir $previous_release
     [[ (-d "dist") ]] && mv dist $previous_release
     [[ (-d "build") ]] && mv build $previous_release
-    [[ (-d "release") ]] && mv release $previous_release
 fi
 
 # Delete current build archives
 rm -rf build dist
-mkdir release
 
 # Make sure you're running this inside a virtual environment
 pip install -r ./requirements.txt
