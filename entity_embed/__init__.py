b33081e968af31e2e57635fef37f78dc52d03fd9
Flávio Juvenal <flavio@vinta.com.br>
2021-02-25 10:51:05 -0300

    Fix notebooks with new AttrInfoDictParser

--- entity_embed/__init__.py
+++ entity_embed/__init__.py
@@ -4,6 +4,7 @@ import logging
 # libgomp issue, must import n2 before torch. See: https://github.com/kakao/n2/issues/42
 import n2  # noqa: F401
 
+from .data_utils.helpers import AttrInfoDictParser  # noqa: F401
 from .data_utils.numericalizer import default_tokenizer  # noqa: F401
 from .entity_embed import *  # noqa: F401, F403
 
