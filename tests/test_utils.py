d736e1388be39bf0e14bc7c1124642ae2cade235
rvlb-19 <renatovlbarros19@gmail.com>
2021-03-02 13:45:19 -0300

    Add more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
 import pytest
-from entity_embed.data_utils.utils import compute_max_str_len
+from entity_embed.data_utils.utils import (
+    compute_max_str_len,
+    compute_vocab_counter,
+    id_pairs_to_cluster_mapping_and_dict,
+)
 
 
 @pytest.fixture
@@ -51,3 +55,46 @@ def test_compute_max_str_len_is_multitoken_true(attr_val_gen):
 def test_compute_max_str_len_is_multitoken_true_without_callable_tokenizer_raises(attr_val_gen):
     with pytest.raises(TypeError):
         compute_max_str_len(attr_val_gen=attr_val_gen, is_multitoken=True, tokenizer=None)
+
+
+def test_compute_max_str_len_is_multitoken_tokenizer_returns_none(attr_val_gen):
+    max_str_len = compute_max_str_len(
+        attr_val_gen=attr_val_gen,
+        is_multitoken=True,
+        tokenizer=lambda x: None,
+    )
+
+    assert max_str_len == 0
+
+
+def test_compute_vocab_counter(attr_val_gen):
+    vocab_counter = compute_vocab_counter(
+        attr_val_gen=attr_val_gen,
+        tokenizer=lambda x: x.split(),
+    )
+    assert dict(vocab_counter) == {"foo": 2, "product": 2, "the": 1, "from": 1, "world": 1}
+
+
+def test_id_pairs_to_cluster_mapping_and_dict():
+    id_pairs = {
+        (1, 2),
+        (2, 3),
+        (4, 5),
+        (6, 7),
+        (7, 8),
+        (7, 9),
+        (9, 10),
+    }
+    cluster_mapping, cluster_dict = id_pairs_to_cluster_mapping_and_dict(id_pairs)
+
+    # 1, 2, 3 are part of the same cluster
+    assert len(set(v for k, v in cluster_mapping.items() if k in [1, 2, 3])) == 1
+
+    # 4, 5 are part of the same cluster
+    assert len(set(v for k, v in cluster_mapping.items() if k in [4, 5])) == 1
+
+    # 6, 7, 8, 9, 10 are part of the same cluster
+    assert len(set(v for k, v in cluster_mapping.items() if k in [6, 7, 8, 9, 10])) == 1
+
+    clusters_set = set(tuple(sorted(c)) for c in cluster_dict.values())
+    assert clusters_set == {(1, 2, 3), (4, 5), (6, 7, 8, 9, 10)}
