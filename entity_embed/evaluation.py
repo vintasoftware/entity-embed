3c836aae60ea72ead31b823d27ab0cc68328f18b
Flávio Juvenal <flavio@vinta.com.br>
2021-04-06 12:29:02 -0300

    Include example-data in repo

--- entity_embed/evaluation.py
+++ entity_embed/evaluation.py
@@ -1,3 +1,7 @@
+import csv
+import json
+
+
 def pair_entity_ratio(found_pair_set_len, entity_count):
     return found_pair_set_len / entity_count
 
@@ -24,3 +28,28 @@ def f1_score(precision, recall):
         return (2 * precision * recall) / (precision + recall)
     else:
         return 0.0
+
+
+def evaluate_output_json(
+    unlabeled_csv_filepath, output_json_filepath, pos_pair_json_filepath, csv_encoding="utf-8"
+):
+    with open(
+        unlabeled_csv_filepath, "r", newline="", encoding=csv_encoding
+    ) as record_dict_csv_file:
+        record_count = sum(1 for __ in csv.DictReader(record_dict_csv_file))
+
+    with open(output_json_filepath, "r") as f:
+        found_pair_set = json.load(f)
+    found_pair_set = set(tuple(t) for t in found_pair_set)
+
+    with open(pos_pair_json_filepath, "r") as f:
+        pos_pair_set = json.load(f)
+    pos_pair_set = set(tuple(t) for t in pos_pair_set)
+
+    precision, recall = precision_and_recall(found_pair_set, pos_pair_set)
+    return (
+        precision,
+        recall,
+        f1_score(precision, recall),
+        pair_entity_ratio(len(found_pair_set), record_count),
+    )
