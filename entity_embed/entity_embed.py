d23f0c2e15995eff752db9cc9194925bfdd64a5e
Flávio Juvenal <flavio@vinta.com.br>
2021-05-24 12:54:58 -0300

    use_vicreg=False

--- entity_embed/entity_embed.py
+++ entity_embed/entity_embed.py
@@ -29,6 +29,7 @@ class _BaseEmbed(pl.LightningModule):
         learning_rate=1e-5,
         ann_k=10,
         sim_threshold_list=[0.4, 0.6, 0.8],
+        use_vicreg=True,
         index_build_kwargs=None,
         index_search_kwargs=None,
         **kwargs,
@@ -47,6 +48,7 @@ class _BaseEmbed(pl.LightningModule):
         self.mu = 25
         self.nu = 1
         self.small_val = 0.0001
+        self.use_vicreg = use_vicreg
         self.learning_rate = learning_rate
         self.ann_k = ann_k
         self.sim_threshold_list = sim_threshold_list
@@ -85,34 +87,44 @@ class _BaseEmbed(pl.LightningModule):
         # similarity loss
         sim_loss = self.sim_loss_fn(embeddings, labels)
 
-        # invariance loss
-        anchor_idx, pos_idx, __, __ = lmu.get_all_pairs_indices(labels)
-        z_a = embeddings[anchor_idx]
-        z_b = embeddings[pos_idx]
-        inv_loss = F.mse_loss(z_a, z_b)
+        if self.use_vicreg:
+            # Based on: https://paperswithcode.com/paper/vicreg-variance-invariance-covariance
 
-        # variance loss
-        std_z_a = torch.sqrt(z_a.var(dim=0) + self.small_val)
-        std_z_b = torch.sqrt(z_b.var(dim=0) + self.small_val)
-        std_loss = torch.mean(F.relu(1 - std_z_a)) + torch.mean(F.relu(1 - std_z_b))
+            # invariance loss
+            anchor_idx, pos_idx, __, __ = lmu.get_all_pairs_indices(labels)
+            z_a = embeddings[anchor_idx]
+            z_b = embeddings[pos_idx]
+            inv_loss = F.mse_loss(z_a, z_b)
 
-        # covariance loss
-        N, D = embeddings.size()
-        z_a = z_a - z_a.mean(dim=0)
-        z_b = z_b - z_b.mean(dim=0)
-        cov_z_a = (z_a.T @ z_a) / (N - 1)
-        cov_z_b = (z_b.T @ z_b) / (N - 1)
-        cov_loss = (
-            cov_z_a.fill_diagonal_(0).pow_(2).sum() / D
-            + cov_z_b.fill_diagonal_(0).pow_(2).sum() / D
-        )
+            # variance loss
+            std_z_a = torch.sqrt(z_a.var(dim=0) + self.small_val)
+            std_z_b = torch.sqrt(z_b.var(dim=0) + self.small_val)
+            std_loss = torch.mean(F.relu(1 - std_z_a)) + torch.mean(F.relu(1 - std_z_b))
 
-        loss = self.lbda * sim_loss + self.lbda * inv_loss + self.mu * std_loss + self.nu * cov_loss
+            # covariance loss
+            N, D = embeddings.size()
+            z_a = z_a - z_a.mean(dim=0)
+            z_b = z_b - z_b.mean(dim=0)
+            cov_z_a = (z_a.T @ z_a) / (N - 1)
+            cov_z_b = (z_b.T @ z_b) / (N - 1)
+            cov_loss = (
+                cov_z_a.fill_diagonal_(0).pow_(2).sum() / D
+                + cov_z_b.fill_diagonal_(0).pow_(2).sum() / D
+            )
 
-        self.log("train_sim_loss", sim_loss)
-        self.log("train_inv_loss", inv_loss)
-        self.log("train_std_loss", std_loss)
-        self.log("train_cov_loss", cov_loss)
+            loss = (
+                self.lbda * sim_loss
+                + self.lbda * inv_loss
+                + self.mu * std_loss
+                + self.nu * cov_loss
+            )
+            self.log("train_sim_loss", sim_loss)
+            self.log("train_inv_loss", inv_loss)
+            self.log("train_std_loss", std_loss)
+            self.log("train_cov_loss", cov_loss)
+        else:
+            loss = sim_loss
+
         self.log("train_loss", loss)
         return loss
 
